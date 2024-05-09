f41eef9d43779e8ec6cc74e2071fabd655731b69
alejandroacedomarin <alejandro.acedo@estudiantat.upc.edu>
2024-05-09 11:29:42 +0200

    Primer

---
+++ lib/app.ts
@@ -0,0 +1,23 @@
+import express = require("express");
+
+const app = express();
+const PORT = process.env.PORT || 4000;
+const server = app.listen(PORT,()=>{
+    console.log('Server port: ',PORT);
+});
+const io = require('socket.io')(server);
+const connectedUser= new Set();
+io.on('connection',(socket)=>{
+    console.log("Connected: ",socket.id);
+    connectedUser.add(socket.id);
+    io.emit('connected-user', connectedUser.size);
+    socket.on('disconect',()=>{
+        console.log("Disconnected: ", socket.id);
+        connectedUser.delete(socket.id);
+        io.emit('connected-user', connectedUser.size);
+    });
+    socket.on('message', (data)=>{
+        console.log(data);
+        socket.broadcast.emit('message-receive', data);
+    });
+});
