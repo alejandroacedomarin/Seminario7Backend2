88a46ce9f499cc2c29171c2bd79fcfb87149f573
mariiomc <101208018+mariiomc@users.noreply.github.com>
2024-05-10 10:53:59 +0200

    rooms

--- lib/app.ts
+++ lib/app.ts
@@ -2,22 +2,44 @@ import express = require("express");
 
 const app = express();
 const PORT = process.env.PORT || 4000;
-const server = app.listen(PORT,()=>{
-    console.log('Server port: ',PORT);
+const server = app.listen(PORT, () => {
+    console.log('Server port: ', PORT);
 });
+
 const io = require('socket.io')(server);
-const connectedUser= new Set();
-io.on('connection',(socket)=>{
-    console.log("Connected: ",socket.id);
+const connectedUser = new Set();
+
+
+var room = "";
+
+
+
+io.on('connection', (socket) => {
+ 
+    console.log("Connected: ", socket.id);
+       
     connectedUser.add(socket.id);
-    io.emit('connected-user', connectedUser.size);
-    socket.on('disconect',()=>{
+        app.get('/:room', (req, res) => {
+            room = req.params.room;
+            // Realizar cualquier acción necesaria con el nombre de la sala
+            res.send(`Joining room: ${room}`);
+        });
+    socket.join(room, () => {    
+        console.log(`Socket ${socket.id} joining ${room}`);
+    });
+
+    socket.on('disconect', () => {
         console.log("Disconnected: ", socket.id);
         connectedUser.delete(socket.id);
         io.emit('connected-user', connectedUser.size);
     });
-    socket.on('message', (data)=>{
+
+    socket.on('message', (data) => {
         console.log(data);
-        socket.broadcast.emit('message-receive', data);
+        console.log('estas enviado este mensaje a:', socket.room,data);
+        socket.to(room).emit('message-receive', data);
+        
+        //socket.broadcast.emit('message-receive', data);
     });
 });
+
