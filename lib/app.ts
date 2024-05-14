1c86edab2391811e3f7b2ae0c72146ae91aa4537
borjafernandez35 <borjaf29@gmail.com>
2024-05-14 10:45:45 +0200

    rooms

--- lib/app.ts
+++ lib/app.ts
@@ -7,10 +7,14 @@ const server = app.listen(PORT,()=>{
 });
 const io = require('socket.io')(server);
 const connectedUser= new Set();
+
+
 io.on('connection',(socket)=>{
-    console.log("Connected: ",socket.id);
+    
+    socket.join("room1");
+    console.log("Connected: ",socket.id, socket.rooms);
     connectedUser.add(socket.id);
-    io.emit('connected-user', connectedUser.size);
+    io.to("room1").emit('connected-user', connectedUser.size);
     socket.on('disconect',()=>{
         console.log("Disconnected: ", socket.id);
         connectedUser.delete(socket.id);
@@ -18,6 +22,8 @@ io.on('connection',(socket)=>{
     });
     socket.on('message', (data)=>{
         console.log(data);
-        socket.broadcast.emit('message-receive', data);
+        console.log('estas enviando este mensaje a:',socket.rooms, data);
+        //socket.broadcast.emit('message-receive', data);
+        socket.to("room1").emit('message-receive', data);
     });
 });
